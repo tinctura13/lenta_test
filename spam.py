d4eb2929c5941849077ea66f13bfc4f72a617942
Vladimir Semerikov <51379705+tinctura13@users.noreply.github.com>
2021-03-31 13:37:31 +0500

    Add files via upload

--- spam.py
+++ spam.py
@@ -1,21 +1,33 @@
+import sys
 from string import punctuation as punct
 
-spam = []
+input_file = sys.argv[0]
 
-# remove spam
-for sentence in open('data.csv', 'r', encoding='utf8'):
-    sentence = ''.join([char for char in sentence if char not in punct])
-    for word in sentence.split():
-        # check if word is just digits
-        if not word.isdigit():
-            for char in word:
-                if char.isdigit():
-                    spam.append(sentence)
-                    break
 
-# non spam
-with open('result.csv', 'w', encoding='utf') as f:
-    for sentence in open('data.csv', 'r', encoding='utf8'):
-        new_sentence = ''.join([char for char in sentence if char not in punct])
-        if new_sentence not in spam:
-            f.write(sentence)
+def primitive_spam_filter():
+    """
+    Primitive script that works on heuristics and pretend to filter spam
+    messages from the mail. Spam messages considered as words with integers
+    inside them.
+
+        :return: It saves all messages identified as not spam to 'result.csv'
+    """
+    spam = []
+
+    # define spam
+    for sentence in open(input_file, 'r', encoding='utf8'):
+        sentence = ''.join([char for char in sentence if char not in punct])
+        for word in sentence.split():
+            # check if word is just digits
+            if not word.isdigit():
+                for char in word:
+                    if char.isdigit():
+                        spam.append(sentence)
+                        break
+
+    # select non spam
+    with open('result.csv', 'w', encoding='utf') as f:
+        for sentence in open('data.csv', 'r', encoding='utf8'):
+            new_sentence = ''.join([char for char in sentence if char not in punct])
+            if new_sentence not in spam:
+                f.write(sentence)
